7111e38d3ae8c1687a86b5bc7fe18b45c1dfe998
joaobejarano <joaom.bejarano@gmail.com>
2025-06-19 17:21:01 -0300

    Creating the main structure

---
+++ MainSolution/src/chess/main.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+def main():
+    # PART 1
+    
+
+    # PART 2
+    
+
+    # PART 3
+    
+
+    if __name__ == '__main__':
+        main()
